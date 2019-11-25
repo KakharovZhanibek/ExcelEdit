023a311d4fc0d202200edfcb3169d37f5b76fd6d
z.kontra <z.kontra@mail.ru>
2019-11-25 17:27:47 +0600

    Добавьте файлы проекта.

---
+++ Excel_/ExcelPackageExtension.cs
@@ -0,0 +1,39 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excel_
+{
+    public static class ExcelPackageExtension
+    {
+
+        public static DataTable ToDataTable(this ExcelPackage package)
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.First();
+            DataTable Dt = new DataTable();
+
+            foreach (var firstRowCell in worksheet
+                .Cells[1, 1, 1, worksheet.Dimension.End.Column])
+            {
+                Dt.Columns.Add(firstRowCell.Text);
+            }
+
+            for (int rowNumber = 2; rowNumber <= worksheet.Dimension.End.Row; rowNumber++)
+            {
+                var row = worksheet.Cells[rowNumber, 1, rowNumber, worksheet.Dimension.End.Column];
+                var newRow = Dt.NewRow();
+                foreach (var cell in row)
+                {
+                    newRow[cell.Start.Column-1] = cell.Text;
+                }
+
+                Dt.Rows.Add(newRow);
+            }
+            return Dt;
+        }
+    }
+}
---
+++ Excel_/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Excel_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenExcel(object sender, RoutedEventArgs e)
+        {
+            FileInfo file = new FileInfo(tbxExcelPath.Text);
+            if ((tbxExcelPath.Text.EndsWith(".xlsx") || tbxExcelPath.Text.EndsWith(".xls")) && file.Exists==true)
+            {
+                ExcelPackage package = new ExcelPackage(file);
+                DataTable dt = ExcelPackageExtension.ToDataTable(package);
+                lvExcel.DataContext = dt;
+            }
+            else
+            {
+                MessageBox.Show("Файл не найден");
+            }
+        }
+    }
+}
